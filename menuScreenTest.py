717b4a511b336db21e75ba11e1c15cd7a43f7db0
tailaiwang <tailai.link2001@gmail.com>
2017-06-04 08:45:45 -0400

    Add files via upload

--- menuScreenTest.py
+++ menuScreenTest.py
@@ -1,23 +1,24 @@
 #LoadingScreenTest
 from pygame import*
 screen=display.set_mode((800,600))
-screen.fill((0,0,0))
-buttonRect=Rect(250,100,75,75)
-draw.rect(screen,(255,0,0),buttonRect)
+screen.fill((0,0,0)) #fills screen
+buttonRect=Rect(250,100,75,75) #start Button
+draw.rect(screen,(255,0,0),buttonRect) 
 display.flip()
 menu=True
-while menu:
+while menu: #event loop for menu
     leftClick=False
     running=False
-    for evt in event.get():
+    for evt in event.get(): #checks events
         if evt.type==MOUSEBUTTONDOWN:
             if evt.button==1:
                 leftClick=True
     mx,my=mouse.get_pos()
     if buttonRect.collidepoint((mx,my)) and leftClick:
+        #if mouse clicks on button
         print("running")
-        menu=False
-        running=True
+        menu=False #starts game
+        running=True 
 
 while running:
     print("RUnning")
