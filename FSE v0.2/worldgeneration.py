5fe6314ded3d91e95ec10547b33899b879d0a969
CluelessSquare <leo2001chen@gmail.com>
2017-06-06 13:38:48 -0400

    Update worldgeneration.py
    Sigh

--- FSE v0.2/worldgeneration.py
+++ FSE v0.2/worldgeneration.py
@@ -5,17 +5,21 @@ anchor=20
 for i in range(780):
     x=randint(-3,3)
     anchor+=x
-    if anchor<=0:
+    if anchor<=6:
         anchor+=3
+    if anchor>=79:
+        anchor-=3
     temp=np.zeros((85,1),dtype="int")
     temp[anchor:].fill(1)
     temp[anchor]=3
     for j in range(randint(5,30)):
-        temp[anchor+randint(10,35)]=2
+        try:
+            temp[anchor+randint(10,35)]=2
+        except IndexError:
+            continue
 
     pieces.append(temp)
 
-    
 blocks=np.concatenate(pieces,axis=1)
 ##print(blocks)
 
