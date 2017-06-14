411e000a3b20ce37953cbd823c4c48f207e0cac8
CluelessSquare <leo2001chen@gmail.com>
2017-06-14 14:00:43 -0400

    Changed the size

--- FSE v0.2/player/character.py
+++ FSE v0.2/player/character.py
@@ -5,7 +5,7 @@ from glob import glob
 colour=Color(171,181,198)
 names,hair=[],[]
 x=glob("hair/*.png")
-anchor=0
+
 for i,j in zip(x,range(1,len(x)+1)):
     names.append("Hair {0}".format(j))
     hair.append(image.load(i))
@@ -15,17 +15,18 @@ screen.fill((255,255,255))
 bar=image.load("Hue.png")
 andy=font.Font("HW ANDY.ttf",50)
 clock=time.Clock()
-
+anchor=0
 text=andy.render(names[anchor],1,colour)
 # h=transform.scale(transform.chop(hair[anchor],(0,0,40,56)),(200,280))
 # h=transform.chop(hair[anchor],(0,0,40,56)).convert_alpha()
-h=transform.scale(hair[anchor],(400,7840))
-h.fill((220, 141, 50), special_flags=BLEND_SUB)
-x=transform.scale(h,(200,280))
+# h=transform.scale(hair[anchor],(400,7840))
+h=hair[anchor].subsurface(0,0,40,56).copy()
+
+
 # h=transform.scale(Surface((40,56),hair[anchor]),(200,280))
 #--------------------------RECT---------------------------------
+textRect=Rect((499,208),text.get_size())
 barRect=Rect((478,298),bar.get_size())
-hairRect=Rect((499,208),text.get_size())
 
 def updatecolour():
     global anchor,colour,text
@@ -34,9 +35,11 @@ def updatecolour():
     screen.fill((255,255,255))
     screen.blit(bar, (478,298))
     screen.blit(andy.render(names[anchor],1,(0,0,0)),(499,208))
-    h = transform.scale(hair[anchor], (400, 7840))
-    h.fill(Color(255, 255, 255) - colour, special_flags=BLEND_SUB)
-    screen.blit(h, (375, -75), (0, 0, 400, 560))
+    h = hair[anchor].subsurface(0, 0, 40, 56).copy()
+
+    h.fill(Color(255,255,255)-colour, special_flags=BLEND_SUB)
+    screen.blit(h, (530,65))
+
 def updatehair():
     global anchor,colour,h
     anchor += 1
@@ -44,9 +47,9 @@ def updatehair():
         screen.fill((255, 255, 255))
         screen.blit(bar, (478,298))
         screen.blit(andy.render(names[anchor], 1, (0, 0, 0)), (499,208))
-        h = transform.scale(hair[anchor], (400, 7840))
+        h = hair[anchor].subsurface(0, 0, 40, 56).copy()
         h.fill(Color(255,255,255)-colour, special_flags=BLEND_SUB)
-        screen.blit(h, (375, -75), (0, 0, 400, 560))
+        screen.blit(h, (530,65))
     except IndexError:
         anchor=0
 
@@ -55,19 +58,20 @@ running= True
 
 screen.blit(bar,(478,298))
 screen.blit(andy.render(names[anchor],1,(0,0,0)),(499,208))
-screen.blit(h,(375,-75),(0,0,400,560))
+screen.blit(h,(530,65))
 # scren.blit()
 while running:
-
     leftClick=False
     rightClick=False
     mx, my = mouse.get_pos()
     mb = mouse.get_pressed()
-    print(mx,my)
+    # print(mx,my)
     if barRect.collidepoint(mx,my) and mb[0]==1:
         updatecolour()
-    if hairRect.collidepoint(mx,my) and mb[0]==1:
+        print("updatecolour")
+    if textRect.collidepoint(mx,my) and mb[0]==1:
         updatehair()
+        print("updatehair")
     for evt in event.get():
         if evt.type == QUIT:
             running = False
@@ -81,3 +85,5 @@ while running:
     clock.tick(10)
 
 quit()
+
+
