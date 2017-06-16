a5fb15e267246ccb93e2f4ccde4aa917e7f315de
Tailai Wang <tailai.link2001@gmail.com>
2017-06-15 23:45:14 -0400

    Fully Updated 06/15/2017

--- FSE v0.2/FSE.py
+++ FSE v0.2/FSE.py
@@ -5,7 +5,7 @@ from time import time as tm
 from pygame import *
 import math
 init()
-
+######################################################################
 with open("blockspickle.pickle", "rb") as f:
     blockList = pickle.load(f)
 
@@ -26,8 +26,8 @@ playerSurface = Surface((worldSize[0] * 16, worldSize[1] * 16), SRCALPHA)
 playerSurface.fill((0, 0, 0, 0))
 uiSurface = Surface((1280, 720), SRCALPHA)
 mainSurface = Surface((1280, 720), SRCALPHA)
-
-background = image.load("background.png").convert(32, SRCALPHA)
+###########################################################################
+background = image.load("background.png").convert()
 background = transform.scale(background, (1280, 720))
 
 pics = []
@@ -47,12 +47,17 @@ for i in range(6, 1126, 56):
 
 pics = [pics[0:5], [pics[5], pics[5]], pics[6:19], pics[19:24], [pics[24], pics[24]], pics[25:]]
 
+slimePics=[]
+for i in range(4):
+    name="slime/slime_"+str(i)+".png"
+    slimePics.append(image.load(name))
+#/////////////////////////////////////////////////////////////////////////    
 tile_crack_1 = image.load("tile_cracks/tile_crack_2.png").convert(32, SRCALPHA)
 tile_crack_2 = image.load("tile_cracks/tile_crack_8.png").convert(32, SRCALPHA)
 tile_crack_3 = image.load("tile_cracks/tile_crack_14.png").convert(32, SRCALPHA)
 tile_crack_4 = image.load("tile_cracks/tile_crack_20.png").convert(32, SRCALPHA)
 tile_cracks = [tile_crack_4, tile_crack_3, tile_crack_2, tile_crack_1]
-
+#/////////////////////////////////////////////////////////////////////////
 block1_0 = image.load("dirt/dirt_block_19.png").convert(32, SRCALPHA)
 block1_1 = image.load("dirt/dirt_block_1.png").convert(32, SRCALPHA)
 block1_2 = image.load("dirt/dirt_block_0.png").convert(32, SRCALPHA)
@@ -71,7 +76,7 @@ block1_14 = image.load("dirt/dirt_block_12.png").convert(32, SRCALPHA)
 block1_15 = image.load("dirt/dirt_block_9.png").convert(32, SRCALPHA)
 block1 = [block1_0, block1_1, block1_2, block1_3, block1_4, block1_5, block1_6, block1_7, block1_8, block1_9, block1_10,
           block1_11, block1_12, block1_13, block1_14, block1_15]
-
+#///////////////////////////////////////////////////////////////////////////
 block2_0 = image.load("stone/stone_block_19.png").convert(32, SRCALPHA)
 block2_1 = image.load("stone/stone_block_1.png").convert(32, SRCALPHA)
 block2_2 = image.load("stone/stone_block_0.png").convert(32, SRCALPHA)
@@ -108,12 +113,12 @@ block3_12 = image.load("grass/grass_block_56.png").convert(32, SRCALPHA)
 block3_13 = image.load("grass/grass_block_8.png").convert(32, SRCALPHA)
 block3_14 = image.load("grass/grass_block_12.png").convert(32, SRCALPHA)
 block3_15 = image.load("grass/grass_block_9.png").convert(32, SRCALPHA)
-
+#//////////////////////////////////////////////////////////////////////
 block3 = [block3_0, block3_1, block3_2, block3_3, block3_4, block3_5, block3_6, block3_7, block3_8, block3_9, block3_10,
           block3_11, block3_12, block3_13, block3_14, block3_15]
 blockImg = [False, block1, block2, block3]
 blockConditions = [False, 150, 500, 150]
-
+#/////////////////////////////////////////////////////////////////////
 item1 = image.load("items/item_1.png").convert(32, SRCALPHA)
 item2 = image.load("items/item_2.png").convert(32, SRCALPHA)
 item4 = image.load("items/item_4.png").convert(32, SRCALPHA)
@@ -123,7 +128,87 @@ items = [False, item1, item2, item1, item4]
 toolSpeeds = [5, 5, 5, 5, 15]
 effTools = [0.5, 0.5, 4, 0.5]
 dropsList = []
+#########################################################################
+class Slime:
+    def __init__(self, x, y, w, h):
+        self.rect = Rect(x, y, w, h)
+        self.blitPos = [ x - 8, y - 7]
+        self.vx = 0
+        self.vy = 0
+        self.jumping = False
+        self.right = False
+        self.left = False
+        self.move = -1
+        self.frame = 0
+    def moveSlime(self):
+        distance = abs(player.rect.x - slime.rect.x)
+        x = randint(1,60)
+        if x==60:
+            if distance < 500: 
+                if self.rect.x  <  player.rect.x and not self.jumping:
+                    self.vx = 7
+                    self.vy = -7
+                    self.jumping = True
+                    self.right = True
+                if self.rect.x > player.rect.x and not self.jumping:
+                    self.vx = -7
+                    self.vy= -7
+                    self.jumping = True
+                    self.left = True
+    def collide(self):
+        self.rect.x += self.vx
+        for x in range(self.rect.centerx // 16 - 1, self.rect.centerx // 16 + 2):
+            for y in range(self.rect.centery // 16 - 2, self.rect.centery // 16 + 3):
+                if blocks[y][x].id != 0 and self.rect.colliderect(blocks[y][x].rect):
+                    if self.vx > 0:
+                        self.rect.right = blocks[y][x].rect.left
+                    elif self.vx < 0:
+                        self.rect.left = blocks[y][x].rect.right
+        self.rect.y += self.vy            
+        for x in range(self.rect.centerx // 16 - 1, self.rect.centerx // 16 + 2):
+            for y in range(self.rect.centery // 16 - 2, self.rect.centery // 16 + 3):
+                if blocks[y][x].id != 0 and self.rect.colliderect(blocks[y][x].rect):
+                    self.vy = 5
+                    if self.vy > 0:
+                        self.rect.bottom = blocks[y][x].rect.top
+                        self.jumping = False
+                    elif self.vy < 0:
+                        self.rect.top = blocks[y][x].rect.bottom
 
+        if self.right and self.vx > 0:
+            self.vx -= 0.7
+        else:
+            self.vx = 0
+            self.jumping = False
+            self.right = False
+
+        if self.left and self.vx <0:
+            self.vx += 0.7
+        else:
+            self.vx = 0
+            self.jumping = False
+            self.left = False
+        if self.vy < 30:
+            self.vy += 0.7
+        if self.vy == 0:
+            self.jumping = False
+        
+
+        self.blitPos = [self.rect.x - 8, self.rect.y - 7]
+        
+    def clear(self):
+        draw.rect(playerSurface, (0, 0, 0, 0), (self.blitPos[0] - 50, self.blitPos[1] - 50, 150, 150))
+        
+    def draw(self):
+        x=randint(1,5)
+        if x==5:
+            if self.frame<3:
+                self.frame+=1
+            else:
+                self.frame=0
+        pic = slimePics[int(self.frame)]
+        playerSurface.blit(pic,self.blitPos)
+#########################################################################
 class Player:
     def __init__(self, x, y, w, h):
         self.rect = Rect(x, y, w, h)
@@ -219,7 +304,7 @@ class Player:
         pic = pics[self.move][int(self.frame)]
         playerSurface.blit(pic, self.blitPos)
 
-
+#############################################################################
 class Block:
     def __init__(self, id, x, y):
         self.id = id
@@ -366,7 +451,7 @@ class Block:
                     self.condition = 0
                 else:
                     self.condition -= 5
-
+##############################################################################
 class inventory:
     selected = 0
     def __init__(self, pos, id, quantity, type):
@@ -418,7 +503,7 @@ class Drop:
 
     def draw(self):
         playerSurface.blit(items[self.id], (self.rect.x, self.rect.y))
-
+###########################################################################
 
 def drawBlocks(x1, x2, y1, y2):
     for x in range(x1, x2 + 1):
@@ -438,15 +523,23 @@ for i in range(10):
     inventoryList.append(inventory(i, inventoryPickleList[i][0], 1, inventoryPickleList[i][1]))
 
 drawBlocks(0, len(blocks[0]) - 1, 0, len(blocks) - 1)
-
+###########################################################################
 player = Player(629, 339, 24, 40)
+
+slimeList=[]
+for i in range(5):
+    slime= Slime(randint(700,1100),339,14,14)
+    slimeList.append(slime)
+
+
 EMPTY = 0
 BLOCK = 1
 TOOL = 2
 deleteList = []
-
+##########################################################################
 running = True
-
+positive = True
+time = 0
 while running:
     leftClick = False
     rightClick = False
@@ -492,7 +585,21 @@ while running:
                 inventory.selected = 8
             elif evt.key == K_0:
                 inventory.selected = 9
+#///////////////////////////////////////////////////////////////////////////
+    if time <= 5184000 and positive:
+        time+=5000
+    else:
+        positive = False
+    if time >= 0 and not positive:
+        time-=5000
+    else:
+        positive = True
 
+    alphaCount=time//20330
+    screen.fill((0, 0, 0))
+    background.set_alpha(alphaCount)
+    screen.blit(background,(0,0))
+#///////////////////////////////////////////////////////////////////////////
     if mb[0] == 1:
         blocks[(player.rect.y - 339 + my) // 16][(player.rect.x - 629 + mx) // 16].breakBlock()
         for x in range((player.rect.x - 629 + mx) // 16 - 1, (player.rect.x - 629 + mx) // 16 + 2):
@@ -517,7 +624,11 @@ while running:
             drop.collide()
             drop.clear()
 
-
+    for slime in slimeList:
+        slime.moveSlime()
+        slime.collide()
+        slime.clear()
+        
     player.movePlayer()
     player.collide()
     player.clear()
@@ -527,12 +638,15 @@ while running:
             drop.draw()
         dropsList = [drop for drop in dropsList if not drop.delete]
 
+    for slime in slimeList:
+        slime.draw()
+
     player.draw()
 
     for item in inventoryList:
         item.draw()
 
-    screen.blit(background, (0, 0))
+
     if player.rect.y >= 339:
         screen.blit(blocksSurface.subsurface(player.rect.x - 629, player.rect.y - 339, 1280, 720), (0, 0))
         screen.blit(playerSurface.subsurface(player.rect.x - 629, player.rect.y - 339, 1280, 720), (0, 0))
